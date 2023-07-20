a81aeca762b22bbcc4dd49c769c9487827105182
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-20 16:50:31 -0700

    test/removed testing from App

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,15 +1,9 @@
 import './App.css';
-import cleanUpData from "../../utils"
-import artData from "../../mockData"
 
 function App() {
 
-  const cleanedData = cleanUpData(artData);
-
   return (
-    <div className="App">
-      <p>{JSON.stringify(cleanedData)}</p>
-        
+    <div className="App">        
     </div>
   );
 }
