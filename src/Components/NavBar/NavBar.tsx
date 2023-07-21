63090576d501dd48ee0368675eb1b402ea4c17f1
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-20 20:06:17 -0700

    feat/add styling to NavLinks

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -8,6 +8,8 @@ const NavBar = () => {
     <>
       <nav className='nav-container'>
         <h1>Dilettante's Muse</h1>
+        <NavLink to="/" className="home">Home</NavLink>
+        <NavLink to="/saved" className="saved-art">Saved Art</NavLink>
       </nav>
     </>
   )
