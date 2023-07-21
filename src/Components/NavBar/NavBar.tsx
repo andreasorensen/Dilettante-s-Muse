a65d638a7fa32ebc0bf787f2f30fa80e4f12cdf3
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-21 08:32:33 -0700

    feat/add routes to App component for HomePage & SavedPage

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -7,11 +7,13 @@ const NavBar: React.FC = () => {
     <>
       <nav className='nav-container'>
         <h1>Dilettante's Muse</h1>
-        <NavLink to="/" className="home">Home</NavLink>
+        <NavLink to="/" className="home-page">Home</NavLink>
         <NavLink to="/saved" className="saved-art">Saved Art</NavLink>
       </nav>
     </>
   )
-}
+};
+
+// replace h1 with logo? 
 
 export default NavBar
