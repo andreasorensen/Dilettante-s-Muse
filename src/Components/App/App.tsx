4aa9005dbf0c5c6d81ae8a50dd470e9a1a1bd5e7
“Saki <sakisandra.c@gmail.com>
2023-07-21 10:12:40 -0600

    Limit render art pieces to 3

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -3,32 +3,43 @@ import getArt from '../../ApiCalls';
 import { objectID, cleanUpData, generateID } from '../../utils';
 import {useEffect, useState} from 'react';
 import { ArtData } from '../../utils';
+import React from 'react';
 
 function App() {
 // console.log(getArt)
 // console.log(objectID)
 const [hasImage, setHasImage] = useState(false)
-// const [err, setErr] = useState(false)
+// // const [err, setErr] = useState(false)
 
-const [pieces, setPieces] = useState<ArtData[]>([])
+// {
+//   data: 
+//     { 
+//     id: 2,
+//     title: "This Test", 
+//     artistDisplayName: "Test",
+//     objectDate: 1999,
+//     primaryImage: "https://images.metmuseum.org/CRDImages/ad/web-large/52601.jpg" 
+//     },
+//     isFavorite: false
+// }
+
+const [pieces, setPieces] = useState<any[]>([])
+//change this from any
 
 useEffect(()=> {
   const callApi = () => {
     getArt(generateID())
       .then(data => {
         if (data.primaryImage && data.message !== "ObjectID not found") {
-          const cleanData: any = cleanUpData(data)
+          const cleanData: any = {data: cleanUpData(data), isFavorite: false}
           console.log('DATA WITH IMAGE', cleanData)
-          setPieces(prev => [...prev, data])
+          setPieces(prev => [...prev, cleanData])
           return cleanData
-
         } else {
           setHasImage(prev => !prev)
           const cleanData: any = cleanUpData(data)
           console.log('DATA WITHOUT', cleanData)
         }
-
-
       })
   }
 
@@ -42,14 +53,14 @@ useEffect(()=> {
 },[pieces])
 
 const renderPieces = () => {
-  const allPieces = pieces?.map(piece => <p key={Date.now() + pieces.indexOf(piece)}> {piece.title}</p>)
+  const allPieces = pieces?.map(piece => <p key={Date.now() + pieces.indexOf(piece)}> {piece.data.title}</p>)
   return allPieces.slice(0, 3)
+  //add error handling for if only 2 pieces show up
 }
 
   return (
     <div className="App">      
       {pieces && renderPieces()}
-      //add error handling for when there are only two pieces for some reason
     </div>
   );
 }
