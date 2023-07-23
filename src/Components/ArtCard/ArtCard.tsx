6a0faac78edf1ed97953e561945e976ceaaa9457
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-23 10:45:46 -0700

    add frame & background to ArtCard

--- src/Components/ArtCard/ArtCard.tsx
+++ src/Components/ArtCard/ArtCard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './ArtCard.css';
 import FaveButton from '../FaveButton/FaveButton';
 import { ArtData }from '../../utils'
+import frame from '../../images/frame.png';
 
 
 interface Props {
@@ -16,17 +17,22 @@ const ArtCard: React.FC<Props> = ({pieces, setPieces, setSavedPieces, piece}: Pr
   const [isFavorited, setIsFavorited ] = useState(false)
 
   return (
-    <div className='art-card'>
-      <div className='image-container'>
-        <img className='image' src={piece.primaryImage} alt={piece.title}/>
-        <FaveButton piece={piece} setSavedPieces={setSavedPieces} setPieces={setPieces} artPiece={isFavorited} pieces={pieces} setArtPiece={setIsFavorited } />
-      </div>
-      <div className='details-container'>
-        <p className='title'>{piece.title}</p>
-        <p className='artist'>{piece.artistDisplayName}</p>
-        <p className='date'>{piece.objectDate}</p>
+    <>
+    <div className='art-frame'>
+      <img className='frame-image' src={frame} alt="frame" />
+      <div className='art-card'>
+        <div className='image-container'>
+          <img className='image' src={piece.primaryImage} alt={piece.title}/>
+          <FaveButton piece={piece} setSavedPieces={setSavedPieces} setPieces={setPieces} artPiece={isFavorited} pieces={pieces} setArtPiece={setIsFavorited } />
+        </div>
+        <div className='details-container'>
+          <p className='title'>{piece.title}</p>
+          <p className='artist'>{piece.artistDisplayName}</p>
+          <p className='date'>{piece.objectDate}</p>
+        </div>
       </div>
     </div>
+  </>
   )
 }
 
