370cb97c0fe158c5861249eb8339c8f5d0b87ff5
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-25 11:35:42 -0700

    added 5XX error handling to callApi(), getIDs() & getRandomIds()

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -3,30 +3,43 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../Homepage/Homepage";
-import {cleanUpData, ArtData, getRandomIds } from "../../utils";
+import { cleanUpData, ArtData, getRandomIds } from "../../utils";
 import SavedPage from "../SavedPage/SavedPage";
 import NavBar from "../NavBar/NavBar";
 import { getArt, getIDs } from "../../apiCalls";
 
+interface Ids {
+  total: number;
+  objectIDs: number[];
+}
 
 function App() {
   const [pieces, setPieces] = useState<ArtData[]>([]);
   const [savePieces, setSavePieces] = useState<ArtData[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   const callApi = async () => {
-    const ids = await getIDs() 
-    const randomPieces = getRandomIds(ids)
-    randomPieces.forEach(id =>
-      getArt(id).then((data) => {
-        console.log('data', data)
+    try {
+      const ids: Ids = await getIDs();
+      console.log("ids:", ids);
+      const randomPieces: number[] = getRandomIds(ids);
+      console.log("randomPieces", randomPieces);
+      for (const id of randomPieces) {
+        const data = await getArt(id);
         if (data.primaryImage && data.message !== "ObjectID not found") {
           const cleanData: any = cleanUpData(data);
           setPieces((prev) => [...prev, cleanData]);
-          return cleanData;
         }
-      })
-    )
-  }
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error && error.message.includes("Server error")) {
+        setError(
+          "Sorry, we encountered a server error. Please try again later."
+        );
+      }
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     if (pieces.length < 3) {
@@ -48,8 +61,7 @@ function App() {
         const copy = [...prev];
         copy.splice(i, 1);
         return copy;
-          
-      })
+      });
     }
   };
 
