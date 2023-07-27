e19f271f027eddceddcbf3d4da44e5466b34c485
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-26 18:10:11 -0700

    test/5XX error loading homepage art pieces

--- cypress/e2e/error-handling.cy.ts
+++ cypress/e2e/error-handling.cy.ts
@@ -1,10 +1,12 @@
 /// when there is a server error, the user is shown a message telling them so
 
-   // -- initial fetch 
+   // -- initial fetch   DONE
+
+   // -- while loading the homepage art pieces   
 
    // -- while navigating to saved
 
-// when the user attempts to go to a nonexistent route, they are taken to another page, giving them a 404 error.
+/// when the user attempts to go to a nonexistent route, they are taken to another page, giving them a 404 error.
 
 describe("Server-side error handling", () => {
 
@@ -20,6 +22,23 @@ describe("Server-side error handling", () => {
     cy.contains("Sorry, we encountered a server error. Please try again later.").should("be.visible");
   })
 
-  
+  it('Handles 5XX errors when loading homepage art pieces', () => {
 
-})
+    cy.intercept('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/search?medium=Paintings&q=painting', {
+      statusCode: 200, 
+      body:  { objectIDs: [1, 2, 3]}  
+    })
+
+    cy.intercept(
+      'GET', "https://collectionapi.metmuseum.org/public/collection/v1/objects/*", {
+        statusCode: 500,
+        body: "Internal Server Error"
+      }).as("getArt");
+  
+    cy.visit('http://localhost:3000/');
+  
+    cy.contains("Loading...").should("be.visible");
+  
+    cy.contains("Sorry, we encountered a server error. Please try again later.").should("be.visible");
+  })
+});
